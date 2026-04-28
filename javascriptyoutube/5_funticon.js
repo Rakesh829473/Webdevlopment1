5454600106c2f45651f4c05e1acfef776a4cb483
Rakesh829473 <rk5271628@gmail.com>
2026-04-28 22:41:35 +0530

    javascript

---
+++ javascriptyoutube/5_funticon.js
@@ -0,0 +1,13 @@
+function sum(a,b)
+{
+    return a+b;
+}
+
+console.log(sum(6,9));
+
+function greet(a,b)
+{
+    console.log("Hello[Rakesh], welcome to thapa Technical js Course");
+}
+
+greet("vinod","ram");
