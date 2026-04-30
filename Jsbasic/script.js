44a2580ce6d3fb2c35e7cf2a03e514e741875c08
Rakesh829473 <rk5271628@gmail.com>
2026-04-30 18:16:16 +0530

    forrloop

--- Jsbasic/script.js
+++ Jsbasic/script.js
@@ -1,13 +1,33 @@
-console.log("hello world");
+// console.log("hello world");
 
 
-let a=10;
-console.log(a)
+// let a=10;
+// console.log(a)
 
-let b=20;
+// let b=20;
 
-a=10;
+// a=10;
 
-var x=10;
+// var x=10;
 
-var x=20;
+// var x=20;
+ var n=10;
+for(var i=1;i<=n;i++)
+{
+    console.log(2+ "*"+i+"="+2*i);
+
+}
+
+let a=[24,58,33,86,96,12,53,23,32,100]
+
+for(var i=0;i<10;i++)
+{
+    console.log(a[i]);
+}
+
+let j=0;
+while(j<10)
+{
+    console.log(a[j]);
+    j++;
+}
