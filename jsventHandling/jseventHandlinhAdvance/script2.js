30bd3caed0acf8a0b34967881101b8ecc55efdba
Rakesh829473 <rk5271628@gmail.com>
2026-05-09 22:37:42 +0530

    event

--- jsventHandling/jseventHandlinhAdvance/script2.js
+++ jsventHandling/jseventHandlinhAdvance/script2.js
@@ -1,19 +1,33 @@
-document.querySelector("from").addEventListener("submit",(event)) =>{
-    event.preventDefault();
-}
-const fn=document.getElementById("FullName").ariaValueMax;
-const em=document.getElementById("email").ariaValueMax;
-const ps=document.getElementById("password").ariaValueMax;
-const dataPacket ={
-    FullName:fn,
-    Email:em,
-    Password:ps,
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-};
-console.log(dataPacket);
-Document.getElementById("FullName").value ="";
-Document.getElementById("email").value ="";
-Document.getElementById("password").value ="";
+  const fn = document.getElementById("FullName").value;
+  const em = document.getElementById("email").value;
+  const ps = document.getElementById("password").value;
 
+  const dataPacket = {
+    FullName: fn,
+    Email: em,
+    Password: ps,
+  };
 
+  console.log(dataPacket);
 
+  document.getElementById("FullName").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+});
+
+document.querySelector("form").addEventListener("reset", (event) => {
+  event.preventDefault();
+
+  confirm("Are Your Sure") && window.location.reload();
+  //   if (confirm("Are your Sure")) {
+  //     window.location.reload();
+  //   }
+});
+
+document.addEventListener("keydown", (event) => {
+  let keyPress = event.key;
+  keyPress === "Escape" && window.location.reload();
+});
